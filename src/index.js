9749567bd66500fa33a27d861ffd47a6fdd0ab62
JohannesSchreurs <johannes@basedesign.com>
2019-09-09 13:29:48 +0200

    Added some test files that don't need to be pushed to git

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './styles/styles.scss';
-import AppRouter from './routers/AppRouter'
+//import AppRouter from './routers/AppRouter';
+import Test from './components/test';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+ReactDOM.render(<Test />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
