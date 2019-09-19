314fd927465a707c566a9316630c67718617d929
JohannesSchreurs <johannes@basedesign.com>
2019-09-19 13:00:08 +0200

    Now users only see their own expenses

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({startLogout }) => {
     return (
         <header>
             <h1>Budget Tracker</h1>
-            <NavLink to="/" activeClassName="is-active" exact>Dashboard</NavLink>
+            <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
             <NavLink to="/create" activeClassName="is-active">Create an expense</NavLink>
             <button onClick={startLogout}>Log out</button>
         </header>
